f5730e8dfdb27f956c2756cd2564afdada2ddb19
AZHARRRRRRRRR <143793259+AZHARRRRRRRRR@users.noreply.github.com>
2023-09-15 16:53:25 +0800

    Update main.py
    update coding

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 def jadualdarab():
-    for x in range (79):
+    for x in range(95):
         print("*" , end="")
     print()
 
-    print("\t\t\t\t\t\t\t\tJADUAL DARAB")
+    print("\t\t\t\t\t\t\t\t\t\tJADUAL DARAB")
 
-    for x in range (79):
+    for x in range(95):
         print("*" , end="")
     print()
 
-    for i in range (1, 11):
-        print("\n", i)
-        for j in range (1, 11):
-            j
+    print("\t", end="")
 
+    for i in range (1, 13):
+        print(f"\t{i}\t", end="")
+    print()
+
+    for j in range (1, 13):
+        print(f"{j}\t\t", end="")
+        for i in range(1, 13):
+            result = i * j
+            print(f"{result}\t\t", end="")
         print()
+
 
 
 if __name__ == "__main__":
